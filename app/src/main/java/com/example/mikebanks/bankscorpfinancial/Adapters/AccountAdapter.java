71ff3c02337f38cb19606b98e9f204573a3194a4
phuc <nminhphuc99@gmail.com>
2022-05-24 20:50:32 +0700

    init

---
+++ app/src/main/java/com/example/mikebanks/bankscorpfinancial/Adapters/AccountAdapter.java
@@ -0,0 +1,54 @@
+package com.example.mikebanks.bankscorpfinancial.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.mikebanks.bankscorpfinancial.Model.Account;
+import com.example.mikebanks.bankscorpfinancial.R;
+
+import java.util.ArrayList;
+
+
+public class AccountAdapter extends ArrayAdapter<Account> {
+
+    private Context context;
+    private int resource;
+
+    public AccountAdapter(Context context, int resource, ArrayList<Account> accounts) {
+        super(context, resource, accounts);
+
+        this.context = context;
+        this.resource = resource;
+    }
+
+
+    @Override
+    @NonNull
+    public View getView (int position, View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null) {
+
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            convertView = inflater.inflate(resource, parent, false);
+        }
+
+        Account account = getItem(position);
+
+        TextView txtAccountName = convertView.findViewById(R.id.txt_account_name);
+        txtAccountName.setText(account.getAccountName());
+
+        TextView txtAccountNo = convertView.findViewById(R.id.txt_acc_no);
+        txtAccountNo.setText(context.getString(R.string.account_no) + " " + account.getAccountNo());
+
+        TextView txtAccountBalance = convertView.findViewById(R.id.txt_balance);
+        txtAccountBalance.setText("Account balance: $" + String.format("%.2f",account.getAccountBalance()));
+
+        return convertView;
+    }
+}
